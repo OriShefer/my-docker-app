f8efca2dc627623277ed2693575a0d6b2d79177b
OriShefer <ori150403@gmail.com>
2026-03-31 13:40:28 +0300

    read /etc/hostname - fails on windows

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 const http = require('http')
-const { createCanvas } = require('canvas')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-  const canvas = createCanvas(200, 200)
-  const ctx = canvas.getContext('2d')
-  ctx.fillStyle = '#0080ff'
-  ctx.fillRect(0, 0, 200, 200)
-  ctx.fillStyle = '#ffffff'
-  ctx.font = '30px sans-serif'
-  ctx.fillText('Hello Docker!', 30, 110)
-  res.writeHead(200, { 'Content-Type': 'image/png' })
-  res.end(canvas.toBuffer())
+  const hostname = fs.readFileSync('/etc/hostname', 'utf8')
+  res.end('Hello from: ' + hostname)
 })
 
 server.listen(3000, () => {
