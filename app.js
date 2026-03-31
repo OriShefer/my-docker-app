70a3f6801539cc752568d8c6f8d0fae7e22fc332
OriShefer <ori150403@gmail.com>
2026-03-31 13:31:45 +0300

    add sharp dependency

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const http = require('http')
+const sharp = require('sharp')
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from Docker! 🐳')
+  sharp({
+    create: {
+      width: 100,
+      height: 100,
+      channels: 3,
+      background: { r: 0, g: 128, b: 255 }
+    }
+  })
+    .png()
+    .toBuffer()
+    .then(buffer => {
+      res.writeHead(200, { 'Content-Type': 'image/png' })
+      res.end(buffer)
+    })
 })
 
 server.listen(3000, () => {
